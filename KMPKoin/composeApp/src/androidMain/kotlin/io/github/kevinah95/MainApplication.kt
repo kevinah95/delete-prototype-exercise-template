5929f692913a678500a83cf10004c75522fec6a9
Kevin A. Hernández Rostrán <kevinah95@gmail.com>
2025-07-11 12:50:09 -0600

    feat: implement MainApplication for Koin initialization and update MainViewController for Koin setup

---
+++ KMPKoin/composeApp/src/androidMain/kotlin/io/github/kevinah95/MainApplication.kt
@@ -0,0 +1,6 @@
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initKoin()
+    }
+}
--- KMPKoin/composeApp/src/iosMain/kotlin/io/github/kevinah95/MainViewController.kt
+++ KMPKoin/composeApp/src/iosMain/kotlin/io/github/kevinah95/MainViewController.kt
@@ -2,4 +2,4 @@ package io.github.kevinah95
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(configure = { initKoin() }) { App() }
